26f4acb80cea9ab08cb18ffbb1d7a386b8377782
nguyenan12 <nguyenann1204@gmail.com>
2026-03-05 23:04:10 +0700

    config rudex persist

--- redux/store.js
+++ redux/store.js
@@ -1,9 +1,32 @@
 // https://redux-toolkit.js.org/tutorials/quick-start
 import { configureStore } from '@reduxjs/toolkit'
-import { ObjectReducer } from './user/ObjectSlice'
+import { ObjectReducer } from './Object/ObjectSlice'
 
+
+import { combineReducers } from '@reduxjs/toolkit'
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+//cau hinh persist
+/*********************************************************************/
+const rootPersistConfig = {
+  key: 'root',
+  storage: storage,
+  whitelist: ['Object']
+}
+
+//combine cac reduer trong project
+const reducers = combineReducers({
+  activeBoard: activeBoardReducer,
+  Object: ObjectReducer
+})
+
+//perssist reducer
+const persistedReducer = persistReducer(rootPersistConfig, reducers)
+/*********************************************************************/
 export const store = configureStore({
   reducer: {
     Object: ObjectReducer
   }
+  //reducer: persistedReducer
 })

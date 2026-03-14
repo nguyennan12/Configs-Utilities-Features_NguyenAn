acfe3afc37f06e837c2dac435a2eaa0f6ac27bc7
nguyenan12 <nguyenann1204@gmail.com>
2026-03-14 14:05:05 +0700

    multer and cloundinary

---
+++ cloundinary/config.js
@@ -0,0 +1,30 @@
+import cloudinary from 'cloudinary'
+import streamifier from 'streamifier'
+import { env } from '~/config/environment'
+
+
+const cloudinaryV2 = cloudinary.v2
+cloudinaryV2.config({
+  cloud_name: env.CLOUDINARY_CLOUND_NAME,
+  api_key: env.CLOUDINARY_API_KEY,
+  api_secret: env.CLOUDINARY_API_SECRET
+})
+
+const streamUpload = (fileBuffer, folderName) => {
+  return new Promise((resolve, reject) => {
+    // Tạo luồng upload lên cloudinary
+    const stream = cloudinaryV2.uploader.upload_stream(
+      { folder: folderName },
+      (err, result) => {
+        if (err) reject(err)
+        else resolve(result)
+      }
+    )
+
+    // Thực hiện upload luồng bằng streamifier
+    streamifier.createReadStream(fileBuffer).pipe(stream)
+  })
+
+}
+
+export const CloundinaryProvider = { streamUpload }
